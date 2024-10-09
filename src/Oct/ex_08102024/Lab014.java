702db4cae1020c1f4a4fe66aa90dca9c04f4c5b4
Omkar Kondekar <omkarkondekar1998@gmail.com>
2024-10-09 20:05:02 +0530

    09102024

--- src/Oct/ex_08102024/Lab014.java
+++ src/Oct/ex_08102024/Lab014.java
@@ -4,6 +4,15 @@ public class Lab014 {
 
     public static void main(String[] args) {
 
+            // Store age of Human  = byte, short int, long
+           byte age = 122; // Max  age of human 122
 
+            // Print the age
+            System.out.println("The age of the person is: " +age );
+
+            //Phone Number
+
+        long Phone_Number = 9887766554L;
+        System.out.println("My Mobile No is   "  + Phone_Number);
     }
 }
