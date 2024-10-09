b1fbb20912230da31a06a5ebadafa5692b279504
Omkar Kondekar <omkarkondekar1998@gmail.com>
2024-10-09 20:17:10 +0530

    09102024

---
+++ src/Oct/ex_08102024/Lab017.java
@@ -0,0 +1,18 @@
+package Oct.ex_08102024;
+
+public class Lab017 {
+
+    public static void main(String[] args) {
+
+        char Grade ='A'; // Always single Character in single inverted comma;
+
+        float f =3.14F;
+
+        float f1 =2.14f;
+         double d= 9.9865765;
+
+        //Non Primitive data type
+
+        String name = "Omkar";
+    }
+}
