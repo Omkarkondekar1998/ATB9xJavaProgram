77ba6cf709a82ef4b102f1901e29608f09ea896e
Omkar Kondekar <omkarkondekar1998@gmail.com>
2024-10-27 20:11:36 +0530

    Commit

---
+++ src/Oct/Lab118.java
@@ -0,0 +1,17 @@
+package Oct;
+
+public class Lab118 {
+
+    public static void main(String[] args) {
+        // for singhe character string is not used, it id used for bunch of characters.
+
+        char c = 'D';
+
+        System.out.println(c);
+
+        String gh = "Name";
+
+        System.out.println(gh.length());
+
+    }
+}
---
+++ src/Oct/ex_24102024/Lab119.java
@@ -0,0 +1,17 @@
+package Oct.ex_24102024;
+
+public class Lab119 {
+
+    // How strings are created.
+    String name = "Omkar";
+
+    // String -> dat type
+    // name-> Reference
+    // = -> Assignment Operator
+    // "Omkar" -> Literal (String)
+
+    String name2 = new String("Omkar");
+
+
+}
+
