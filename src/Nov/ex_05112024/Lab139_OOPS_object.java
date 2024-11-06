7974f1f7a389bfe23d5746462cfdd667815e88e5
Omkar Kondekar <omkarkondekar1998@gmail.com>
2024-11-06 21:31:04 +0530

    Commit

---
+++ src/Nov/ex_05112024/Lab139_OOPS_object.java
@@ -0,0 +1,8 @@
+package Nov.ex_05112024;
+
+public class Lab139_OOPS_object {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Nov/ex_05112024/StudentsATB.java
@@ -0,0 +1,33 @@
+package Nov.ex_05112024;
+
+// CAB Rule
+// Class (C) -> here class is StudentsATB
+public class StudentsATB {
+
+// Attribute (A) -> named As - Attribute, Data Members, Instance Variables, fields, member variables
+
+    String name;
+    String Batch_no;
+    Long Phone_number;
+    boolean is_male;
+    boolean is_fees_paid;
+    String Githublink;
+
+    // Behaviour (B) -> named as Behaviour, methods, member functions
+
+    void study(){
+
+    } // no return type no Arguments
+    void eat(int a){
+
+    } // no return type with arguments
+
+    int walk(){
+        return 10; // with return no arguments
+    }
+
+    int sleep(int a){
+        return 10; // with return type and arguments
+    }
+
+}
